70427a570d118d280ac618c8218bd2d6816e3fcc
ujwalkarki <deepblue.uk@gmail.com>
2016-07-01 14:09:31 -0600

    query splitter modified

--- ICDB/src/main/java/main/args/ConvertQueryCommand.java
+++ ICDB/src/main/java/main/args/ConvertQueryCommand.java
@@ -7,10 +7,11 @@ import com.beust.jcommander.Parameters;
 
 import main.args.option.Granularity;
 import main.args.option.GranularityConverter;
+import main.args.option.QuerySPlitter;
 
 /**
  * <p>
- *     JCommander Command for converting a DB query
+ * JCommander Command for converting a DB query
  * </p>
  * Created on 5/10/2016
  *
@@ -19,13 +20,15 @@ import main.args.option.GranularityConverter;
 @Parameters(commandNames = { CommandLineArgs.CONVERT_QUERY }, commandDescription = "Convert queries to ICDB queries")
 public class ConvertQueryCommand extends ConfigCommand {
 
-	@Parameter(names = { "-q", "--query" }, description = "Convert one or more queries as arguments")
+	@Parameter(names = { "-q",
+			"--query" }, description = "Convert one or more queries as arguments", splitter = QuerySPlitter.class)
 	public List<String> queries;
 
 	@Parameter(names = { "-f", "--file" }, description = "Convert all queries in one or more files")
 	public List<String> files;
 
-	@Parameter(names = { "-g", "--granularity" }, converter = GranularityConverter.class, description = "The granularity to use (per tuple or per field)")
+	@Parameter(names = { "-g",
+			"--granularity" }, converter = GranularityConverter.class, description = "The granularity to use (per tuple or per field)")
 	public Granularity granularity = Granularity.TUPLE;
 
 }
